575c71d59abe38d6bf76ef95022db65a3daead33
bayanfuad <bayanjawawdeh9@gmail.com>
2022-08-08 21:39:55 +0300

    authentication

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className='footer'>
+<p className='copy'> copy rights</p>
+            </div>
+        )}}
+export default Footer;
